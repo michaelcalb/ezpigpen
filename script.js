c77d53c22ed4678f359f7af06e1a417d7c161a86
Michael <michael@idealhome.com.br>
2022-10-09 17:44:21 -0300

    basic script

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function hello() {
-    alert('sup')
+// one time letters = "abcdefghistuv"
+// two times letters = "jklmnopqrwxyz"
+
+var result = document.getElementById("result");
+var times = 0;
+
+function pigpen(onetime, twotimes) {
+  times++;
+  if (times >= 3) {
+    times = 0;
+  }
+
+  result.innerHTML = ``;
+
+  if (times != 0 && times % 2 == 0) {
+    result.innerHTML += `${twotimes}`;
+  } else if (times == 0) {
+    return;
+  } else {
+    result.innerHTML += `${onetime}`;
+  }
 }
