9af541adbc1d87cb37339389d246bccf97a8c2a6
Michael <michael@idealhome.com.br>
2022-10-10 15:25:14 -0300

    ITS FINALLY WORKING

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 // one time letters = "abcdefghistuv"
 // two times letters = "jklmnopqrwxyz"
 
-var result = document.getElementById("result");
-var times = 0;
+var result = []
+var text = document.getElementById('text')
 
-function pigpen(onetime, twotimes) {
-  times++;
-  if (times >= 3) {
-    times = 0;
-  }
+function onetime(onetimeletter) {
+    result.push(onetimeletter)
+    text.innerHTML = `decoded: ${result.join('')}`
+}
 
-  result.innerHTML = ``;
-
-  if (times != 0 && times % 2 == 0) {
-    result.innerHTML += `${twotimes}`;
-  } else if (times == 0) {
-    return;
-  } else {
-    result.innerHTML += `${onetime}`;
-  }
+function twotimes(twotimesletter) {
+    result.pop()
+    result.pop()
+    result.push(twotimesletter)
+    text.innerHTML = `decoded: ${result.join('')}`
 }
